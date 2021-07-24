220d563606ab6356272dd867018b36c3bf2e0d28
Michael Arakilian <arakilian0@gmail.com>
2021-07-24 13:43:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-const express = require("express")
-const mongoose = require('mongoose')
-const app = express()
+import express from 'express'
+import mongoose from 'mongoose'
 
-const PORT = process.env.PORT || 3001
+const app = express()
+const port = 3001
+
 mongoose.connect('mongodb://localhost:27017/test', {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server.js /api" })
+app.get('/api', (req, res) => {
+    res.json({ message: "Hello world" })
 })
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
-})
-
-
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+app.listen(
+    port, 
+    console.log(`listening on port ${port}`)
+)
