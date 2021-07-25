f61ecdd1f5d3f0bd3fe275417ac32723e1a373a9
Michael Arakilian <arakilian0@gmail.com>
2021-07-24 23:26:21 -0700

    Create firebaseUIConfig.js

---
+++ client/src/components/Authentication/firebaseUIConfig.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+let firebaseUIConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID
+  ]
+}
+
+export default firebaseUIConfig
