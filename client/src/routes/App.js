f2d253f06d1e1d32e8dd01e50524cdefa8e2bcd3
Michael Arakilian <arakilian0@gmail.com>
2021-07-26 23:20:17 -0700

    Create App.js

---
+++ client/src/routes/App.js
@@ -0,0 +1,9 @@
+import { Component } from 'react'
+import Calendar from '../components/Calendar/Calendar'
+
+export default class AppRoute extends Component {
+    constructor(props) { super(props) }
+
+    render() { return(<Calendar />) }
+}
+  
