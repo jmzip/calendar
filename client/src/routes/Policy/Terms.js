e3c72a02a5ae62a74bafa3483a0de215fc8e3bbe
Michael Arakilian <arakilian0@gmail.com>
2021-07-26 22:34:40 -0700

    Create Terms.js

---
+++ client/src/routes/Policy/Terms.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+export default class TermsRoute extends React.Component {
+    constructor(props) { super(props) }
+
+    render() {
+        return(
+            <Container>
+                <h1>Terms</h1>
+            </Container>
+        )
+    }
+}
