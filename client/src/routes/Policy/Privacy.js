8c612050f86d305a41cb539d5e18ded5221fa9b3
Michael Arakilian <arakilian0@gmail.com>
2021-07-26 22:34:29 -0700

    Create Privacy.js

---
+++ client/src/routes/Policy/Privacy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+export default class PrivacyRoute extends React.Component {
+    constructor(props) { super(props) }
+
+    render() {
+        return(
+            <Container>
+                <h1>Privacy</h1>
+            </Container>
+        )
+    }
+}
