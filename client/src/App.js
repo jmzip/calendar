e3a0395b2687cacccf7acd0feae4d04cc2f76d3f
Michael Arakilian <arakilian0@gmail.com>
2021-07-26 22:32:53 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,67 @@
-import React from "react"
+// React 
+import { Component } from 'react'
+import './App.css'
 
+// Redux
+import { connect } from 'react-redux'
+import { isLoggedIn } from './store/actions'
+
+// Router
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from "react-router-dom"
 
-import "./App.css"
+// Routes
+import HomeRoute from './routes/Home'
+import LoginRoute from './routes/Landing/Login'
+import LogoutRoute from './routes/Logout'
+import SignupRoute from './routes/Landing/Signup'
+import TermsRoute from './routes/Policy/Terms'
+import PrivacyRoute from './routes/Policy/Privacy'
 
-import Header from './components/Header/Header'
-import Login from './components/Authentication/Login'
+// Firebase
+import firebase from 'firebase'
+import { Config } from './firebase'
+firebase.initializeApp(Config)
 
-function Home() {
-  return <h2>Home</h2>
+// App Component
+class App extends Component {
+  constructor(props) { super(props) }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) this.props.isLoggedIn(user.uid)
+    })
+  }
+
+  render() {
+
+    console.log(this.props.isLoggedIn, 'app')
+    return(
+      // React Router
+      <Router>
+          <Switch>
+            {/* START Routes */}
+            <Route path='/terms' component={ TermsRoute } />
+            <Route path='/privacy' component={ PrivacyRoute } />
+            <Route path='/signup' component={ SignupRoute } />
+            <Route path='/login' component={ LoginRoute } />
+            <Route path='/logout' component={ LogoutRoute }></Route>
+            <Route path='/' component={ HomeRoute } />
+            {/* END Routes */}
+          </Switch>
+      </Router>
+    )
+  }
 }
 
-function About() {
-  return <Login />
-}
+// State to Prop Mapping
+const mapStateToProps = state => ({
+  isLoggedIn: state.isLoggedIn
+})
 
-function Users() {
-  return <h2>Users</h2>
-}
-
-export default function App() {
-  const [data, setData] = React.useState(null)
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-  }, [])
-
-  return (
-    <Router>
-      <div>
-
-        <Header /> 
-
-        <Switch>
-          <Route path="/about">
-            <div>
-              <About />
-            </div>
-          </Route>
-          <Route path="/users">
-            <div>
-              <Users />
-              <p>{!data ? "Loading..." : data}</p>
-            </div>
-          </Route>
-          <Route path="/">
-            <div>
-              <Home />
-              <p>{!data ? "Loading..." : data}</p>
-            </div>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  )
-}
+// Connected Export
+export default connect(mapStateToProps, { isLoggedIn })(App)
