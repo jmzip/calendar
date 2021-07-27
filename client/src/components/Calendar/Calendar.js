3af787b6ee691e3d899ea9d1e46b7af2b3e2da8b
Michael Arakilian <arakilian0@gmail.com>
2021-07-26 23:20:06 -0700

    Update Calendar.js

---
+++ client/src/components/Calendar/Calendar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Calendar extends React.Component {
+    constructor(props) { super(props) }
+
+    render() {
+        return(
+            <div>Calendar</div>
+        )
+    }
+}
