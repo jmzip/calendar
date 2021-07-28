868dabe92ae3125b9796f2f9d9754e864932f1c0
Michael Arakilian <arakilian0@gmail.com>
2021-07-27 22:13:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 import express from 'express'
-import mongoose from 'mongoose'
 
 const app = express()
 const port = 3001
-
-mongoose.connect('mongodb://localhost:27017/test', {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-})
 
 app.get('/api', (req, res) => {
     res.json({ message: "Hello world" })
